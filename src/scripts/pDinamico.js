6f35da2fc7fc5d2419b0381406ea6911c221802d
mahojv <moisesmjavier@outlook.com>
2025-03-10 21:29:00 -0600

    Se diseña crea la pagina proyectos.html y se generan los arrays con objetos para la autogeneracion de codigo

---
+++ src/scripts/pDinamico.js
@@ -0,0 +1,101 @@
+console.clear()
+
+const projectsArray = [
+
+    {
+
+        "nombre": "Portafolio",
+        "descripcion": "Portafolio web para presentacion a nuevos clientes",
+        "fecha de creacion": "02/03/2025",
+        "foto": "" //direccion de imagen
+    
+    
+    },
+    {
+
+        "nombre": "curriculum cv - automatizacion",
+        "descripcion": "pagina web de ingeniero mecatronico especializado en automatizacion industrial",
+        "fecha de creacion": "15/02/2025",
+        "foto": "" //direccion de imagen
+    
+    
+    },
+
+]
+
+const skillsArray = [
+    {
+
+        "nombre": "HTML 5",
+        "alt": "HTML 5",        
+        src: "./src/images/html5.png" //direccion de imagen
+    
+    },
+    {
+
+        "nombre": "css 3",
+        "alt": "css 3",        
+        src: "./src/images/css3.png" //direccion de imagen
+    
+    },
+    {
+
+        "nombre": "javascript",
+        "alt": "javascript",        
+        src: "./src/images/JS.png" //direccion de imagen
+    
+    },
+    {
+
+        "nombre": "react",
+        "alt": "react",        
+        src: "./src/images/react.png" //direccion de imagen
+    
+    },
+    {
+
+        "nombre": "Tailwind",
+        "alt": "Tailwind",        
+        src: "./src/images/tailwind.png" //direccion de imagen
+    
+    },
+    {
+
+        "nombre": "Vue.js",
+        "alt": "Vue.js",        
+        src: "./src/images/Vuejs.png" //direccion de imagen
+    
+    }, 
+]
+
+
+const addSkillsArray = [
+    {
+
+        "nombre": "GitHub",
+        "alt": "GitHub",        
+        src: "./src/images/Github.png" //direccion de imagen
+    
+    },
+    {
+
+        "nombre": "NodeJS",
+        "alt": "NodeJS",        
+        src: "./src/images/nodejs.png" //direccion de imagen
+    
+    },
+    {
+
+        "nombre": "Bootstrap",
+        "alt": "Bootstrap",        
+        src: "./src/images/bootstrap.png" //direccion de imagen
+    
+    },
+    {
+
+        "nombre": "Angular",
+        "alt": "Angular",        
+        src: "./src/images/angularjs.png" //direccion de imagen
+    
+    }
+]
